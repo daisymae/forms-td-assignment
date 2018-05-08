f020defc316b0abbdfef4a70fd0047675219fb4e
daisymae <daisymae@orcadiafarm.com>
2018-05-07 23:20:56 -0500

    Provided form as requested
    Add a Form with the following Inputs (and Validators)
               1) Mail address (should not be empty and should be an email address)
               2) A Dropdown which allows the user to select from three different Subscriptions ("Basic", "Advanced", "Pro")
                  Set "Advanced" as Default
               3) A Password field (should not be empty)
               4) A Submit Button
    
               Display a warning message if the Form is invalid AND was touched. Display a warning message below each input if it's invalid.
    
               Upon submitting the form, you should simply print the Value of the Form to the Console.
               Optionally, display it in your template.
    
    Additionally made it so the reset() also reset the default value in the form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') subscriptionForm: NgForm;
+  defaultSubscription = 'advanced';
+  subscription = {
+    email: '',
+    subscription: '',
+    pwd: ''
+  };
+  submitted = false;
+
+  onSubmit() {
+    console.log(this.subscriptionForm);
+    this.submitted = true;
+    this.subscription.email = this.subscriptionForm.value.email;
+    this.subscription.subscription = this.subscriptionForm.value.subscription;
+    this.subscription.pwd = this.subscriptionForm.value.password;
+    console.log(this.subscription);
+    // reset the default value
+    this.subscriptionForm.reset({ subscription: this.defaultSubscription});
+  }
 
 }
